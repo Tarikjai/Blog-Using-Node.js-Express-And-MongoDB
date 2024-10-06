3ad49f311a53f9b9edb84e36d05849e70ede3a2f
Tarik <tarik.jaidani@gmail.com>
2024-10-06 19:28:51 +0200

    rework

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const articleRouter = require('./routes/articles')
-const Article = require('./models/article')
+const express = require("express")
 const app = express()
-const methodOverride = require('method-override');
+const articlesRouter = require('./routes/articles')
 
-mongoose.connect("mongodb://localhost/blog"/*, {useUnifiedTopology: true} , {useNewUrlParser: true}*/)
-
-app.set('view engine', 'ejs' )
-
-app.use(express.urlencoded({ extended: false}))
-app.use(methodOverride('_method'));
+const PORT = 3000
 
 
-app.get('/', async (req,res)=> {
-    const articles = await Article.find().sort({createdAt: 'desc'})
-    res.render('articles/index', {articles: articles})
+app.use('/articles', articlesRouter)
+app.set('view engine','ejs')
 
-
+app.get('/', (req,res)=>{
+    const articles = [{
+        title :"Test Article",
+        createdAt : Date.now(),
+        description: "Test description"
+    },{title :"Test Article",
+        createdAt : Date.now(),
+        description: "Test description"}]
+    res.render('index.ejs', { articles : articles})
 })
 
-app.use('/articles', articleRouter)
-
-
-
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
+app.listen(PORT ,()=>{
+    console.log("Server running on port: "+ PORT)
+})
